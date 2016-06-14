980194eeb2b53675b6eb719492038adf1a1f7871
Lea de Groot <l.degroot@library.uq.edu.au>
2016-06-15 09:31:12 +1000

    new reusable element for Drupal to search in Fryer (PT116103147)

---
+++ README.md
@@ -0,0 +1,22 @@
+# uqlibrary-fryer-search
+
+uqlibrary-fryer-search is a search form for Fryer Library
+
+There is currently no documentation at [GitHub Pages](http://uqlibrary.github.io/uqlibrary-fryer-search).
+
+### Getting Started
+Install Node.JS and run the following oneliner in the project directory:
+```sh
+npm install -g bower && bower install
+```
+
+### Developing
+- Please adhere to the Polymer code style guide provided at [Style Guide](http://polymerelements.github.io/style-guide/). 
+- Colors and common styles are imported (bower install) from [uqlibrary-styles](http://github.com/uqlibrary/uqlibrary-styles).
+- GitHub pages should be updated after every commit to Master by running the "generate-gh-pages.sh" in the /bin/ directory
+
+### Testing
+Tests are run using the Web Component Tester. Either navigate to /tests/index.html in a browser or using the command line:
+```sh
+wct --local all
+```
---
+++ elements/uqlibrary-fryer-search.js
@@ -0,0 +1,118 @@
+(function () {
+  Polymer({
+    is: 'uqlibrary-fryer-search',
+
+    properties: {
+      /**
+       * Application name for google analytics records.
+       */
+      gaAppName: {
+        type: String,
+        value: 'FryerSearch'
+      },
+
+      /*
+       * Search type specified in user call of template.
+       */
+      searchType: {
+        type: String,
+        observer: '_searchTypeChanged'
+      },
+
+      /*
+       * Values for selected search type.
+       * This will be set to one of the values of _sources.
+       */
+      _selectedSource: {
+        type: Object
+      },
+
+      /**
+       * Values for each possible search type.
+       */
+      _sources: {
+        type: Object,
+        value: {
+          summon: {
+            url: 'http://uq.summon.serialssolutions.com/#!/search?q=',
+            urlAppend: '&ho=t&fvf=ContentType,Trade Publication Article,t|Library,Fryer,f|ContentType,Newspaper Article,t|ContentType,Manuscript,f&l=en-AU',
+            searchLabel: 'Search for manuscript material'
+          },
+
+          primo: {
+            url: 'http://search.library.uq.edu.au/primo_library/libweb/action/search.do?vl(freeText0)=',
+            urlAppend: '&fn=search&ct=facet&fctN=facet_library&fctV=61UQ_FRY&rfnGrp=1&rfnGrpCounter=1&frbg=&&indx=1&dscnt=0&vl(1UIStartWith0)=contains&scp.scps=scope%3A(61UQ)%2Cprimo_central_multiple_fe&tb=t&vid=61UQ&mode=Basic&ct=search&srt=rank&tab=61uq_all&vl(D75285834UI0)=any',
+            searchLabel: 'Search for manuscript material'
+          },
+
+          espace: {
+            url: 'http://espace.library.uq.edu.au/list/?search_keys[0]=',
+            urlAppend: '&sort_by=searchKey0&cat=quick_filter&search_keys[core_8]=UQ:278419',
+            searchLabel: 'Search for digitised material'
+          },
+
+          google: {
+           url: 'https://www.google.com.au/#q=',
+           urlAppend: '+site:www.library.uq.edu.au/fryer-library/ms',
+           searchLabel: 'Search all manuscript finding aids'
+         }
+
+        }
+      },
+
+      /*
+       * Makes the Return Key fire search.
+       */
+      _inputKeywordTarget: {
+        type: Object,
+        value: function () {
+          return this.$.searchKeywordInput;
+        }
+      }
+
+    },
+
+    
+    /*
+     * Builds the url and opens it.
+     */
+    _searchActivated: function(e) {
+
+      var searchText = this.$.searchKeywordInput.value;
+
+      var searchUrl = this._selectedSource.url + searchText;
+      if (this._selectedSource.urlAppend) {
+        searchUrl += this._selectedSource.urlAppend;
+      }
+      searchUrl = encodeURI(searchUrl);
+
+      this.async(function () {
+        window.open(searchUrl, '_blank');
+      }, 100);
+
+      this.$.ga.addEvent(this.searchType, searchText);
+      this.$.searchKeywordInput.selectedSuggestion = null;
+    },
+
+
+    /*
+     * Sets the selected source according to the search type.
+     */
+    _searchTypeChanged: function() {
+      if (typeof(this._sources) !== undefined && this._sources.hasOwnProperty(this.searchType)) {
+        this._selectedSource = this._sources[this.searchType];
+      }
+
+    },
+
+
+    ready: function() {
+      // When a valid search type is not provided, defaults to google.
+      if (typeof(this.searchType) !== undefined && this._selectedSource === undefined) {
+        this._selectedSource = this._sources.google;
+      }
+
+    }
+
+  });
+})();
